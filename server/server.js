2923b1b777ae69f257612366ed80e7fe8979d141
Enoma Deborah <deborah.enomah@andela.co>
2017-07-12 10:53:07 +0100

    node app

---
+++ server/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const path = require("path");
+const socketIO = require("socket.io");
+const http = require("http");
+
+const publicPath = path.join(__dirname, "../public");
+const port = process.env.PORT || 3000;
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
+app.use(express.static(publicPath));
+
+io.on("connection", socket => {
+  console.log("New User connected");
+
+  socket.on("createMessage", message => {
+    console.log("Create message", message);
+    io.emit("newMessage", {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User was disconnected");
+  });
+});
+
+server.listen(port, () => {
+  console.log(`server is up on port ${port}`);
+});
